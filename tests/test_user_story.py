b5890111413a511959793487372b226feac24487
Andy Theodorko <andrii.fedorko@gmail.com-os.local>
2022-04-19 23:10:33 +0300

    Test-driven development: add failing test

--- tests/test_user_story.py
+++ tests/test_user_story.py
@@ -1,2 +1,15 @@
+from model import Ingredient
+
+
+def increment(ingredient: Ingredient, new_amount: int):
+    return None
+
+
 def test_nothing():
-    assert 0
+    tomatoes = Ingredient(name="tomatoes", amount=0)
+    assert increment(tomatoes, 1) == 0
+
+
+def test_save():
+    tomatoes = Ingredient(name="tomatoes", amount=0)
+    tomatoes.save()
