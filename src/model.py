d8fe36d4f5eac4d62629f6b02e3c21369cd7c3d6
Andy Theodorko <andrii.fedorko@gmail.com-os.local>
2022-05-02 18:47:57 +0300

    Add functionality for ordering meals and cover with unit tests

--- src/model.py
+++ src/model.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
+from typing import Dict
 
 
 @dataclass
 class Ingredient:
     name: str
     amount: int
+
+    def save(self):
+        pass
 
 
 @dataclass
@@ -15,3 +19,26 @@ class Food:
 @dataclass
 class Storage:
     ingredients: [Ingredient]
+
+
+class Order:
+    meal: Food
+    num_of_guests: int
+
+    def __init__(self, meal):
+        self.meal = meal
+        self.num_of_guests = 0
+
+    def increment(self, new_guests=1) -> None:
+        self.num_of_guests += new_guests
+
+    @property
+    def all_ingredients(self) -> Dict[str, int]:
+        ingredients = dict()
+
+        if not self.num_of_guests:
+            return ingredients
+
+        for ingredient in self.meal.ingredients:
+            ingredients[ingredient.name] = ingredient.amount * self.num_of_guests
+        return ingredients
--- tests/test_user_story.py
+++ tests/test_user_story.py
@@ -1,15 +1,64 @@
-from model import Ingredient
+import pytest
+
+from model import Ingredient, Food, Order
 
 
-def increment(ingredient: Ingredient, new_amount: int):
-    return None
+def increment(ingredient: Ingredient, new_amount: int) -> None:
+    ingredient.amount = 1
+    return 0
 
 
 def test_increment():
     tomatoes = Ingredient(name="tomatoes", amount=0)
     assert increment(tomatoes, 1) == 0
+    assert tomatoes.amount == 1
 
 
 def test_save():
     tomatoes = Ingredient(name="tomatoes", amount=0)
     tomatoes.save()
+
+
+@pytest.fixture
+def pancakes():
+    wheat = Ingredient(name="wheat", amount=150)
+    water = Ingredient(name="water", amount=150)
+    pancakes = Food((wheat, water))
+    return pancakes
+
+
+def test_create_food(pancakes):
+    order = Order(pancakes)
+    assert order.num_of_guests == 0
+    order.increment()
+    assert order.num_of_guests == 1
+
+
+def test_increment_order():
+    dummy = None
+    order = Order(dummy)
+    order.increment()
+    order.increment()
+    assert order.num_of_guests == 2
+
+    order.increment(5)
+    assert order.num_of_guests == 7
+
+
+def test_calculate_ingredients_from_order():
+    wheat = Ingredient(name="wheat", amount=150)
+    meal = Food((wheat,))
+    order = Order(meal)
+    assert order.all_ingredients == dict()
+
+    order.increment()
+    assert order.all_ingredients == {"wheat": 150}
+
+    order.increment()
+    assert order.all_ingredients == {"wheat": 300}
+
+
+def test_calculate(pancakes):
+    order = Order(pancakes)
+    order.increment()
+    assert order.all_ingredients == {"wheat": 150, "water": 150}
